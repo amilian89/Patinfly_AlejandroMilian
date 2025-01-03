35dd87d98bec4e0ce21be95f62849b31fa5b75b5
Alejandro <alejandro.milian@estudiants.urv.cat>
2025-01-03 11:31:18 +0100

    Initial Commit

---
+++ Patinfly_AlejandroMilian/Patinfly_AlejandroMilianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patinfly_AlejandroMilianApp.swift
+//  Patinfly_AlejandroMilian
+//
+//  Created by Alejandro Milian Lago on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Patinfly_AlejandroMilianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Patinfly_AlejandroMilianTests/Patinfly_AlejandroMilianTests.swift
@@ -0,0 +1,17 @@
+//
+//  Patinfly_AlejandroMilianTests.swift
+//  Patinfly_AlejandroMilianTests
+//
+//  Created by Alejandro Milian Lago on 3/1/25.
+//
+
+import Testing
+@testable import Patinfly_AlejandroMilian
+
+struct Patinfly_AlejandroMilianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
