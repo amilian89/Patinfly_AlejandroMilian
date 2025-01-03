939a7031c00fe868cb9a17866dee0afbee5e992d
Alejandro <alejandro.milian@estudiants.urv.cat>
2025-01-03 13:05:32 +0100

    LoginView commit v.2

--- Patinfly_AlejandroMilian/Login/LoginView.swift
+++ Patinfly_AlejandroMilian/Login/LoginView.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var labelText: String = "No selected"
+    
+    @StateObject private var loginModelView = LoginViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            // Logo or Image at the top
+            Image(systemName: "scooter")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding(.top, 60)
+                .foregroundColor(.blue)
+            
+            Text("Welcome Back")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Text("Sign in to continue")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+
+            VStack(spacing: 15) {
+                TextField("Adreça de correu", text: $loginModelView.credentials.email)
+                    .keyboardType(.emailAddress)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10)
+                    .autocapitalization(.none)
+                    .keyboardType(.emailAddress)
+                    .disableAutocorrection(true)
+                
+                SecureField("Contrassenya", text: $loginModelView.credentials.password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                
+                if loginModelView.showProgressView {
+                    ProgressView()
+                }
+                
+                Button("Sign in") {
+                    loginModelView.login { success in
+                        labelText = "Selected"
+                    }
+                }
+                .disabled(loginModelView.loginDisable)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding(.horizontal)
+                
+                TextField("", text: $labelText)
+                    .padding(.horizontal, 80)
+                    .padding(.vertical, 20)
+                    .autocapitalization(.none)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .disabled(loginModelView.showProgressView)
+            }
+        }
+        .padding(.horizontal, 60)
     }
 }
 
-#Preview {
-    LoginView()
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
 }
+
--- Patinfly_AlejandroMilian/Patinfly_AlejandroMilianApp.swift
+++ Patinfly_AlejandroMilian/Patinfly_AlejandroMilianApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Patinfly_AlejandroMilianApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
