444626729769434d934a98233da6f5bcec5f6a6c
Alejandro <alejandro.milian@estudiants.urv.cat>
2025-01-04 13:40:09 +0100

    condiciones de uso

---
+++ Login/Credentials.swift
@@ -0,0 +1,14 @@
+//
+//  Credentials.swift
+//  Patinfly_AlejandroMilian
+//
+//  Created by Alejandro Milian Lago on 3/1/25.
+//
+
+import Foundation
+
+struct Credentials: Codable{
+    
+    var email: String = ""
+    var password: String = ""
+}
